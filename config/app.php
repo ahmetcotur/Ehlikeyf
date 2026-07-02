9fa99aee1e75081de92412a623623e16833c4c1e
ahmetcotur <ahmet@ahmetcotur.com>
2026-07-02 21:57:33 +0300

    fix: remove legacy hardcoded domain name from config/app.php

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://eliakas.com.tr'),
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', env('APP_ENV') === 'production' ? 'https://eliakas.com.tr' : null),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
